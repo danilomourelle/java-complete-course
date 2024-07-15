0b0362e8d522e3e18c0f1cf4c3889b1fc42ef485
Danilo Mourelle <danilomourelle@outlook.com>
2024-07-15 16:27:02 -0300

    implements King and Rook and place pieces

--- src/board/Board.java
+++ src/board/Board.java
@@ -3,13 +3,13 @@ package board;
 public class Board {
   private Integer rows;
   private Integer columns;
-  private Piece[][] pieces;
+  private Piece[][] spots;
 
   public Board(int rows, int columns){
     this.rows = rows;
     this.columns = columns;
 
-    pieces = new Piece[rows][columns];
+    spots = new Piece[rows][columns];
   }
 
   public Integer getRows() {
@@ -28,11 +28,16 @@ public class Board {
     this.columns = columns;
   }
 
-  public Piece piece(int row, int column) {
-    return pieces[row][column];
+  public Piece pieceOnSpot(int row, int column) {
+    return spots[row][column];
   }
   
-  public Piece piece(Position position){
-    return pieces[position.getRow()][position.getColumn()];
+  public Piece pieceOnSpot(Position position){
+    return spots[position.getRow()][position.getColumn()];
+  }
+
+  public void placePiece(Piece piece, Position position){
+    spots[position.getRow()][position.getColumn()] = piece;
+    piece.position = position;
   }
 }
--- src/chess/Match.java
+++ src/chess/Match.java
@@ -1,19 +1,28 @@
 package chess;
 
 import board.Board;
+import board.Position;
+import chess.pieces.King;
+import chess.pieces.Rook;
 
 public class Match {
   private Board board;
 
   public Match() {
     board = new Board(8, 8);
+    initialSetup();
+  }
+
+  private void initialSetup() {
+    board.placePiece(new Rook(board, Color.WHITE), new Position(2,1));
+    board.placePiece(new King(board, Color.BLACK), new Position(0,4));
   }
 
   public ChessPiece[][] getPieces() {
     ChessPiece[][] matrix = new ChessPiece[board.getRows()][board.getColumns()];
     for (int i = 0; i < board.getRows(); i++) {
       for (int j = 0; j < board.getColumns(); j++) {
-        matrix[i][j] = (ChessPiece) board.piece(i, j);
+        matrix[i][j] = (ChessPiece) board.pieceOnSpot(i, j);
       }
     }
 
